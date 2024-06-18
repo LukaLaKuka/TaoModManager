164f9eb19c0deadb00934345effe25918b1c45e8
Tomás Nahuel Antela Rizzo <tomasnahuelantela@gmail.com>
2024-06-18 17:36:51 +0100

    Reset Button designed

--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -5,10 +5,12 @@
 
 <div class="flex h-20 justify-between sticky bg-darker border-b-light border">
     <div class=" grid grid-cols-4 justify-start items-center gap-2">
-        <Logo className=" w-20 col-span-1" />
+        <Logo className="w-20 col-span-1" />
         <h1 class=" text-4xl font-yeon text-light col-span-3">
             Tao Mod Manager
         </h1>
     </div>
-    <PlayButton/>
+    <div class="flex justify-center px-3 gap-3">
+        <PlayButton/>
+    </div>
 </div>
---
+++ src/lib/components/navbar/resetButton/resetButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Reset from "$lib/icons/reset.svelte";
+</script>
+
+<button class="bg-primary flex justify-between items-center gap-2 text-light ps-2 pe-3 rounded-button my-3 transition-all duration-500 hover:bg-primary-darker">
+    <Reset className="w-6" />
+    <span class="font-yeon text-lg min-h-full" style="line-height: 14;">Fix Models</span>
+</button>
